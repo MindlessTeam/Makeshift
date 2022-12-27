634913bffd252389f1cc5193bd8669c9dc7da2b8
Aaron Beardless <ABeardless@gmx.net>
2022-12-27 01:07:34 +0100

    [Organization] Updated gitignore
    - Now excluding the entire out/ folder

--- out/include/Makeshift/Makeshift.hpp
+++
@@ -1,8 +0,0 @@
-// ----------------------------------------------
-// Copyright (c) 2022 Aaron Kerker
-// MIT-Licensed: https://opensource.org/licenses/MIT
-// ----------------------------------------------
-
-#pragma once
-
-#include "Core/Engine.h"
