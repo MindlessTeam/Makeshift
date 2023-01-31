0f5f6780fc24bdc9e8c5c79526da5bb3c848f24f
Aaron Beardless <ABeardless@gmx.net>
2023-01-31 09:07:56 +0100

    [Organization] Prepared for refactor

--- Makeshift/src/Makeshift/Makeshift.hpp
+++
@@ -1,8 +0,0 @@
-// ----------------------------------------------
-// Copyright (c) 2022-2023 Aaron Kerker
-// MIT-Licensed: https://opensource.org/licenses/MIT
-// ----------------------------------------------
-
-#pragma once
-
-#include "Core/Engine.h"
